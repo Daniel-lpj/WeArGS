936addb9f6659d749d224f29400fb95014aa66ee
djunior <dlpjunior@apoioprodesp.sp.gov.br>
2023-05-31 23:13:25 -0300

    Application

---
+++ src/main/java/WeArGS/WeArGSApplication.java
@@ -0,0 +1,12 @@
+package WeArGS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeArGSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeArGSApplication.class, args);
+    }
+}
